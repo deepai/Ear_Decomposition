65378460a2fccd561711eaa844576eb419ecdb7f
Debarshi Dutta <deepai.dutta@gmail.com>
2016-09-24 13:00:02 +0530

    added converter using ogdf. Need to install OGDF in /opt/OGDF
    	modified:   CMakeLists.txt
    	new file:   Codes/Converter.cpp

---
+++ Codes/Converter.cpp
@@ -0,0 +1,12 @@
+#include <ogdf/basic/Graph.h>
+#include <ogdf/basic/GraphAttributes.h>
+#include <ogdf/fileformats/GraphIO.h>
+ 
+using namespace ogdf;
+ 
+int main()
+{
+	Graph G;
+
+	return 0;
+}
