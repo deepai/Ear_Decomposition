a670b5334a1920e2e8e36bc94277adf486153fbb
Debarshi Dutta <deepai.dutta@gmail.com>
2016-09-26 19:19:26 +0530

    added a runner script.
    	new file:   run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+# $1 is the name of the corresponding .mtx file
+# $2 = 1 for weighted coordinates and 0 for unweighted coordinates
+
+#invoke the graph conversion utility
+#bin/graph_csr_cc $2 < data/$1".mtx" > data/$1".txt"
+
+#echo $1 >> results.txt
+
+time_original=$(bin/Ear_Original < data/$1".txt")
+time_our=$(bin/Ear_V2 < data/$1".txt")
+
+echo $1 $time_original $time_our >> results.txt
